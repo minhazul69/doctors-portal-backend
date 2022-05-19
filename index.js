c47d97d6ec9ff37a21ef3a7ac79b03e5ff78bca9
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-05-19 10:34:34 +0600

    admin verify

--- index.js
+++ index.js
@@ -90,14 +90,30 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
-    app.put("/user/admin/:email", async (req, res) => {
+    app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: { role: "admin" },
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      console.log(requester);
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      console.log(requesterAccount);
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
     });
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
